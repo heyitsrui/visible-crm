327f62a2115554a684fd8958d6ecfdc735201e3b
Ritz <187958702+Ritzqtt@users.noreply.github.com>
2026-03-12 18:42:52 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ const LoginView = () => {
     e.preventDefault();
     setError('');
     try {
-      const response = await fetch('http://localhost:5000/login', {
+      const response = await fetch('http://192.168.1.16:5000/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email, password }),
